506f237a83777e809fe946ba96a9762d71b0401b
Justus Beck <jujo03h@icloud.com>
2025-12-12 23:32:48 +0100

    update

--- baseline_00d62c1b.py
+++ baseline_00d62c1b.py
@@ -30,7 +30,7 @@ GENESIZE = vft.GENESIZE
 # Task-specific settings
 TASK_ID = "00d62c1b"
 TRAINING_ITERATIONS = 3000
-LEARNING_RATE = 1e-3 # 5e-3 for 3x3, 1e-3 for 7x7
+LEARNING_RATE = 5e-3 # 5e-3 for 3x3, 1e-3 for 7x7
 STEPS_BETWEEN_ITERATIONS = (20, 31)  # Random range, originally 32,64, now always 10.
 # Curiously, this originally always made 64 steps at eval but at most 63 when training
 EVAL_STEPS = STEPS_BETWEEN_ITERATIONS[1] - 1
