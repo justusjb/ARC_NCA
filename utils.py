bf12ccfb5ca6ec39f551a50d74f11b913cce166b
Justus Beck <jujo03h@icloud.com>
2025-12-15 16:26:56 +0100

    fix

--- utils.py
+++ utils.py
@@ -31,7 +31,8 @@ def get_batch(pool, x_prime, batch_size, noise_level = 0.0):
             batch[:, chn // 2:, ...]) * m1[:,chn//2:,...].float()
 
     # Previously, only first item in batch was reset to seed like this: batch[0:1, :, :, :] = x_prime
-    batch[0:4, :, :, :] = x_prime.unsqueeze(0).repeat(2, 1, 1, 1)
+    n_to_reseed = 4
+    batch[0:n_to_reseed, :, :, :] = x_prime.unsqueeze(0).repeat(n_to_reseed, 1, 1, 1)
     
     return batch, idxs
 
