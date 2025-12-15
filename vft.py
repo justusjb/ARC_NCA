2c6fd8a150fcbd4f8d7f9203845c1f07188d75be
Justus Beck <jujo03h@icloud.com>
2025-12-15 18:07:36 +0100

    update, batch_size 64->8

--- vft.py
+++ vft.py
@@ -1,7 +1,7 @@
 import torch
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 CHANNELS = 20
-BATCH_SIZE = 64
+BATCH_SIZE = 8
 MASKING = 0
 GENESIZE = 0 # set to 0 for non-Engram or weird issues may occur
 GENE_HIDDEN_N = 132
