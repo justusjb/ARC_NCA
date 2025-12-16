6b53b55f730d094c3b8c92d1df94de490804f253
Justus Beck <jujo03h@icloud.com>
2025-12-16 09:19:02 +0100

    update

--- vft.py
+++ vft.py
@@ -1,9 +1,9 @@
 import torch
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-CHANNELS = 20
+CHANNELS = 23
 BATCH_SIZE = 16
 MASKING = 0
 GENESIZE = 0 # set to 0 for non-Engram or weird issues may occur
-GENE_HIDDEN_N = 132
+GENE_HIDDEN_N = 386
 GENE_PROP_HIDDEN_N = 132
 NUM_PROBLEMS = 120
